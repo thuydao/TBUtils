713d6e6eec91de65f502f93d9b0cd90fe515407d
ThuyDD <thuydd@qsoft.com.vn>
2015-01-30 16:09:39 +0700

    something

--- TDMBProgressHUDManager.m
+++ TDMBProgressHUDManager.m
@@ -23,19 +23,23 @@
 
 /// + 1 into coutProgress. When count == 1, show loading
 + (void)showLoading {
+    /*
     [TDMBProgressHUDManager sharedInstance].countProgress += 1;
     if ([TDMBProgressHUDManager sharedInstance].countProgress == 1) {
         [MBProgressHUD showHUDAddedTo:[SCAppDelegate shareAppDelegate].window animated:YES];
     }
+     */
 }
 
 /// - 1 into coutProgress. When count == 0, hide loading
 + (void)hideLoading {
+    /*
     [TDMBProgressHUDManager sharedInstance].countProgress -= 1;
     if ( [TDMBProgressHUDManager sharedInstance].countProgress < 0)  [TDMBProgressHUDManager sharedInstance].countProgress  = 0;
     if ([TDMBProgressHUDManager sharedInstance].countProgress == 0) {
         [MBProgressHUD hideAllHUDsForView:[SCAppDelegate shareAppDelegate].window animated:YES];
     }
+     */
 }
 
 
