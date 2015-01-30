9fa0e7c64ac4ecc29db2bc41054e066b8150def2
ThuyDD <thuydd@qsoft.com.vn>
2015-01-30 16:03:22 +0700

    change

---
+++ TDMBProgressHUDManager.h
@@ -0,0 +1,26 @@
+//
+//  TDMBProgressHUDManager.h
+//  SCP490
+//
+//  Created by thuydd on 1/21/15.
+//  Copyright (c) 2015 QSoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MBProgressHUD.h"
+
+#define HIDELOADING [TDMBProgressHUDManager hideLoading];
+#define SHOWLOADING [TDMBProgressHUDManager showLoading];
+
+
+@interface TDMBProgressHUDManager : NSObject
+
+@property (nonatomic, assign) NSInteger countProgress;
+
++ (instancetype)sharedInstance;
+
++ (void)showLoading;
+
++ (void)hideLoading;
+
+@end
---
+++ TDMBProgressHUDManager.m
@@ -0,0 +1,42 @@
+//
+//  TDMBProgressHUDManager.m
+//  SCP490
+//
+//  Created by thuydd on 1/21/15.
+//  Copyright (c) 2015 QSoft. All rights reserved.
+//
+
+#import "TDMBProgressHUDManager.h"
+
+
+@implementation TDMBProgressHUDManager
+
++ (instancetype)sharedInstance {
+    static TDMBProgressHUDManager *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[TDMBProgressHUDManager alloc] init];
+        sharedInstance.countProgress = 0;
+    });
+    return sharedInstance;
+}
+
+/// + 1 into coutProgress. When count == 1, show loading
++ (void)showLoading {
+    [TDMBProgressHUDManager sharedInstance].countProgress += 1;
+    if ([TDMBProgressHUDManager sharedInstance].countProgress == 1) {
+        [MBProgressHUD showHUDAddedTo:[SCAppDelegate shareAppDelegate].window animated:YES];
+    }
+}
+
+/// - 1 into coutProgress. When count == 0, hide loading
++ (void)hideLoading {
+    [TDMBProgressHUDManager sharedInstance].countProgress -= 1;
+    if ( [TDMBProgressHUDManager sharedInstance].countProgress < 0)  [TDMBProgressHUDManager sharedInstance].countProgress  = 0;
+    if ([TDMBProgressHUDManager sharedInstance].countProgress == 0) {
+        [MBProgressHUD hideAllHUDsForView:[SCAppDelegate shareAppDelegate].window animated:YES];
+    }
+}
+
+
+@end
